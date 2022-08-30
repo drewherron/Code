cac8b6e4e5f8a6c2d639bbee12c4d1d5bd0cdf0f
Drew Herron <herron@mailbox.org>
2022-08-30 00:31:52 -0700

    Two sum

--- C/two_sum.c
+++ C/two_sum.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
-int* two_sum(int* nums, int nums_size, int target)
+// Leetcode #1
+
+int* twoSum(int* nums, int nums_size, int target, int* return_size)
 {
     int* out_array;
 
-    out_array = (int*) calloc(2, sizeof(int));
+    out_array = (int*) calloc(*return_size, sizeof(int));
 
-    for (int i = 0; i > nums_size; i++)
-        for (int j = 0; j > nums_size; i++)
+    for (int i = 0; i < nums_size; i++)
+        for (int j = 0; j < nums_size; j++)
             if (i != j && nums[i] + nums[j] == target)
             {
                 out_array[0] = i;
                 out_array[1] = j;
+                return out_array;
             }
     return out_array;
 }
@@ -23,6 +23,7 @@ int* two_sum(int* nums, int nums_size, int target)
 int main()
 {
     int* in_array;
+    int* return_size;
     int array_size = 0;
     int target = 0;
 
@@ -38,19 +39,25 @@ int main()
     printf("\nEnter target number:\n");
     scanf("%d", &target);
 
-    int* out_array = two_sum(in_array, array_size, target);
+    return_size = (int*) malloc(sizeof(int));
+    *return_size = 2;
 
+    int* out_array = twoSum(in_array, array_size, target, return_size);
+
+    printf("\nArray:\n");
     for (int i = 0; i < array_size; i++)
     {
         printf("%d", in_array[i]);
-        if (i != array_size)
+        if (i != array_size - 1)
             printf(",");
     }
-    if (out_array[0] != 0 || out_array[1] != 0)
-        printf("\nSum of elements %d and %d is %d.\n", out_array[0], out_array[1], target);
+    if (out_array[0] == out_array[1])
+        printf("\nNo two values sum to %d.\n", target);
     else
-        printf("No two values sum to %d.\n", target);
+        printf("\nSum of elements %d and %d is %d.\n", out_array[0] + 1, out_array[1] + 1, target);
         
+    printf("\n");
 
+    free(in_array);
     free(out_array);
 }
