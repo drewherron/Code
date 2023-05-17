ec931208f6ae39f8bc6624fe5c8b4e304100bc2c
Drew Herron <herron@mailbox.org>
2023-05-17 15:56:10 -0700

    Pebble robot memoization

--- c/pebbles-memo.c
+++ c/pebbles-memo.c
@@ -11,28 +11,36 @@
 
 int field[HEIGHT][WIDTH];
 
+int memo[HEIGHT][WIDTH];
+
 char arrows[HEIGHT + WIDTH - 2] = {'x'};
 
 int ask(int y, int x)
 {
-    if (y == 0 && x == 0)
+    if (memo[y][x] != -1)
     {
-        return field[y][x];
+        return memo[y][x];
+    } else {
+        if (y == 0 && x == 0)
+        {
+            return field[y][x];
+        }
+
+        //OR
+        //if (x < 0 || y < 0)
+        //    return 0;
+
+        int a = -1;
+        int b = -1;
+
+        if (x > 0)
+            a = ask(y, x-1);
+        if (y > 0)
+            b = ask(y-1, x);
+
+        memo[y][x] = field[y][x] + max(a, b);
+        return memo[y][x];
     }
-
-    //OR
-    //if (x < 0 || y < 0)
-    //    return 0;
-
-    int a = -1;
-    int b = -1;
-
-    if (x > 0)
-        a = ask(y, x-1);
-    if (y > 0)
-        b = ask(y-1, x);
-
-    return field[y][x] + max(a, b);
 }
 
 int main()
@@ -54,20 +62,39 @@ int main()
     {
         for (int j = 0; j < WIDTH; j++)
         {
-            printf("%d ", field[i][j]);
+            memo[i][j] = -1;
         }
-        printf("\n");
     }
 
     result = ask(HEIGHT-1, WIDTH-1);
 
-    printf("Max pebbles:\t%d\n", result);
+    printf("\nField:\n");
 
-    printf("Using path:\t");
-    for (int i = 0; i < (HEIGHT + WIDTH - 2); i++)
+    for (int i = 0; i < HEIGHT; i++)
     {
-        printf("%c ", arrows[i]);
+        for (int j = 0; j < WIDTH; j++)
+        {
+            printf("%d ", field[i][j]);
+        }
+        printf("\n");
     }
-    printf("\n\n");
+    printf("\nMemo table:\n");
+    for (int i = 0; i < HEIGHT; i++)
+    {
+        for (int j = 0; j < WIDTH; j++)
+        {
+            memo[i][j] == -1 ? printf("1 ") : printf("%d ", memo[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\nMax pebbles:\t%d\n", result);
+
+    //printf("Using path:\t");
+    //for (int i = 0; i < (HEIGHT + WIDTH - 2); i++)
+    //{
+    //    printf("%c ", arrows[i]);
+    //}
+    //printf("\n\n");
     return 0;
 }
