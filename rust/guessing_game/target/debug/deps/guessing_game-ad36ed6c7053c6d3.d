a562364469edd9dd1b2a155560e5b6d2c9009c31
Drew Herron <herron@mailbox.org>
2023-08-29 11:13:38 -0700

    Rust book chapter 2

--- rust/guessing_game/target/debug/deps/guessing_game-ad36ed6c7053c6d3.d
+++
@@ -1,5 +0,0 @@
-/home/drew/Projects/Code/rust/guessing_game/target/debug/deps/guessing_game-ad36ed6c7053c6d3: src/main.rs
-
-/home/drew/Projects/Code/rust/guessing_game/target/debug/deps/guessing_game-ad36ed6c7053c6d3.d: src/main.rs
-
-src/main.rs:
--- rust/guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/home/drew/Projects/Code/rust/guessing_game/target/debug/guessing_game: /home/drew/Projects/Code/rust/guessing_game/src/main.rs
